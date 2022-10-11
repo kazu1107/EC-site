3c753594b48607514c8fd8e9b4002851474c12e0
Tawaraya <kazu0711tawa@gmail.com>
2022-10-11 13:20:23 +0900

    add mypage

--- resources/views/product/user.blade.php
+++ resources/views/product/user.blade.php
@@ -1,16 +1,44 @@
 {{-- @extends('layouts.app') --}}
-<x-guest-layout>
-<x-product.header></x-product.header>
+{{-- <x-guest-layout> --}}
+<x-layout title="USER | EC2">
+    <x-product.header></x-product.header>
 {{-- @section('項目','ユーザー詳細')
 @section('content') --}}
-<section>
-<div class="container">
+{{-- <section> --}}
+<x-layout.single>
+<div class="flex justify-center font-bold text-2xl pb-4">マイページ</div>
+<div class="container flex justify-center mt-3 mb-24">
     @if($authUser)
     @if(!empty($user))
-    <table class="table table-striped table-hover">
-    <thead>
+    <table class="border-collapse rounded border-2 border-stone-200 table table-striped table-hover">
+    {{-- <thead>
+        ここに入れた項目は見出しになる
+    </thead> --}}
     <tr>
-        <th></th>
+        <th class="px-5 py-2 border-2 border-stone-200">ID</th>
+        <td class="px-5 py-2 border-2 border-stone-200">{{ $user->id }}</td>
+    </tr>
+    <tr>
+        <th class="px-5 py-2 border-2 border-stone-200">名前</th>
+        <td class="px-5 py-2 border-2 border-stone-200">{{ $user->name }}</td>
+    </tr>
+    <tr>
+        <th class="px-5 py-2 border-2 border-stone-200">アドレス</th>
+        <td class="px-5 py-2 border-2 border-stone-200">{{ $user->email }}</td>
+    </tr>
+    <tr>
+        <th class="px-5 py-2 border-2 border-stone-200">郵便番号</th>
+        <td class="px-5 py-2 border-2 border-stone-200">{{ $user->post_code }}</td>
+    </tr>
+    <tr>
+        <th class="px-5 py-2 border-2 border-stone-200">住所</th>
+        <td class="px-5 py-2 border-2 border-stone-200">{{ $user->adress }}</td>
+    </tr>
+    <tr>
+        <th class="px-5 py-2 border-2 border-stone-200">電話番号</th>
+        <td class="px-5 py-2 border-2 border-stone-200">{{ $user->tell }}</td>
+    </tr>
+    {{-- <tr>
         <th>ID</th>
         <th>名前</th>
         <th>アドレス</th>
@@ -18,7 +46,6 @@
         <th>住所</th>
         <th>電話番号</th>
     </tr>
-    </thead>
     <tbody>
         <tr>
         <td></td>
@@ -29,12 +56,14 @@
         <td>{{ $user->adress }}</td>
         <td>{{ $user->tell }}</td>
         </tr>
-    </tbody>
+    </tbody> --}}
     </table>
     @endif
     @endif
 </div>
-</section>
+</x-layout.single>
+{{-- </section> --}}
 {{-- @endsection --}}
-<x-product.footer></x-product.footer>
-</x-guest-layout>
+    <x-product.footer></x-product.footer>
+{{-- </x-guest-layout> --}}
+</x-layout>
